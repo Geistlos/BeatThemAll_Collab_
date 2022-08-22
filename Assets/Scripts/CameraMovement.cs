5db16297643dbb1401525591d7957515c466d207
DDC <daniel-de.castro@3wa.io>
2022-08-22 11:52:02 +0200

    suite cameramovement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    [SerializeField] float camSpeed;
+
+    [SerializeField] Vector3 offset = Vector3.zero;
+
+    Vector2 cameraDimension;
+
+    [SerializeField] Camera cam;
+
+    [SerializeField] Transform target;
+
+    [SerializeField] BoxCollider2D cameraBounds;
+
+    bool follow = true;
+
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
+        cameraDimension.y = cam.orthographicSize;
+        cameraDimension.x = cam.orthographicSize * cam.aspect;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +34,13 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 followPosition = new Vector3(target.position.x, 3f, 0f) + offset;
+
+        float minX = cameraBounds.transform.position.x - cameraBounds.size.x / 2 + cameraDimension.x;
+        float maxX = cameraBounds.transform.position.x + cameraBounds.size.x / 2 - cameraDimension.x;
+        followPosition.x = Mathf.Clamp(followPosition.x, minX, maxX);
+
+        Vector3 currentVelocity = Vector3.zero;
+        transform.position = Vector3.SmoothDamp(transform.position, followPosition, ref currentVelocity, Time.deltaTime * camSpeed);
     }
 }
